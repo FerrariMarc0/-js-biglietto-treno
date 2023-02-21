177827e949152cbde6a593e965db2533886c4d4c
FerrariMarc0 <marco1010@live.it>
2023-02-21 19:00:09 +0100

    style

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const priceForKm = 0.21;
 const juniorDiscount = 100 * 20;
 const seniorDiscount = 100 * 40;
 let totPrice = priceForKm * raceKm;
-
+let sum1 = 10
+let sum2 = 10
 if(18 > userAge){
     const sum1 = totPrice - (totPrice / juniorDiscount);
     console.log (`Hai meno di ${userAge} anni e vuoi percorrere ${raceKm}km, ti spetta uno sconto del 20% per un totale di ${sum1.toFixed(2)} euro.`);
@@ -14,3 +15,9 @@ if(18 > userAge){
 } else{
     console.log (`Hai ${userAge} anni e vuoi percorrere ${raceKm}km, il prezzo del biglietto è di ${totPrice.toFixed(2)} euro.`);
 }
+
+document.getElementById("km").innerHTML = raceKm;
+document.getElementById("age").innerHTML = userAge;
+document.getElementById("price").innerHTML = sum1.toFixed(2);
+document.getElementById("price").innerHTML = sum2.toFixed(2);
+document.getElementById("price").innerHTML = totPrice.toFixed(2);
